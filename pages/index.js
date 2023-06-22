0941f988c03ca3e85a438ee488eba89118f7da0e
Pedro Henrique <93086323+pedrohocc@users.noreply.github.com>
2023-06-22 18:52:10 -0300

    adicionando negrito

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function home() {
-    return <h1>Oi minha arquiteta &#128105;, te amo &#128151;</h1>
+    return <h1>Oi minha arquiteta &#128105;. <b>te amo</b> &#128151;</h1>
 }
 
 export default home;
