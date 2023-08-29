62449be81af346a6ec4f84e77e6df6a2f3fed4c6
Pedro Henrique <93086323+pedrohocc@users.noreply.github.com>
2023-08-29 13:53:55 -0300

    `Criando teste de verdade`

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,26 @@
-test('Teste1', () => {
-  console.log('olá mundo')
+const calculadora = require("../models/calculadora.js")
+
+test('Somar 2 + 2 deve retornar 4', () => {
+  const resultado = calculadora.somar(2, 2)
+  expect(resultado).toBe(4)
+})
+
+test('Somar "pedro" + 2 deve retornar "Erro" ', () => {
+  const resultado = calculadora.somar('pedro', 2)
+  expect(resultado).toBe("Erro")
+})
+
+test('Somar 2 + "pedro" deve retornar "Erro" ', () => {
+  const resultado = calculadora.somar(2, 'pedro')
+  expect(resultado).toBe("Erro")
+})
+
+test('Somar "" + "" deve retornar "Erro" ', () => {
+  const resultado = calculadora.somar("", "")
+  expect(resultado).toBe("Erro")
+})
+
+test('Somar null + 2 deve retornar "Erro" ', () => {
+  const resultado = calculadora.somar(null, 2)
+  expect(resultado).toBe("Erro")
 })
